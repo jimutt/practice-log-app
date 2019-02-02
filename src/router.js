4d01b8faf013f41ca234a2ef038fc0ee0397dd57
Jimmy Utterström <jimmy@utterstrom.xyz>
2019-02-02 17:58:42 +0100

    Removed comment

--- src/router.js
+++ src/router.js
@@ -29,13 +29,5 @@ export default new Router({
       name: 'session',
       component: Session
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
   ]
 });
